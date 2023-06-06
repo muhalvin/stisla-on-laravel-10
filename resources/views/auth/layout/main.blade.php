327c97d5cbf99822ab4522f97673cb4ee1fb1e05
Muhammad Alvin <muhammadalvin888@gmail.com>
2023-06-06 21:01:06 +0700

    first commit

---
+++ resources/views/auth/layout/main.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="id">
+
+<head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+    <title>Login &mdash; Stisla</title>
+
+    <!-- General CSS Files -->
+    <link rel="stylesheet" href="{{ url('assets/modules/bootstrap/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ url('assets/modules/fontawesome/css/all.min.css') }}">
+
+    <!-- CSS Libraries -->
+    <link rel="stylesheet" href="{{ url('assets/modules/bootstrap-social/bootstrap-social.css') }}">
+
+    <!-- Template CSS -->
+    <link rel="stylesheet" href="{{ url('assets/css/style.css') }}">
+    <link rel="stylesheet" href="{{ url('assets/css/components.css') }}">
+    <!-- Start GA -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-94034622-3"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'UA-94034622-3');
+    </script>
+    <!-- /END GA -->
+</head>
+
+<body>
+    <div id="app">
+        @yield('main-content')
+    </div>
+
+    <!-- General JS Scripts -->
+    <script src="{{ url('assets/modules/jquery.min.js') }}"></script>
+    <script src="{{ url('assets/modules/popper.js') }}"></script>
+    <script src="{{ url('assets/modules/tooltip.js') }}"></script>
+    <script src="{{ url('assets/modules/bootstrap/js/bootstrap.min.js') }}"></script>
+    <script src="{{ url('assets/modules/nicescroll/jquery.nicescroll.min.js') }}"></script>
+    <script src="{{ url('assets/modules/moment.min.js') }}"></script>
+    <script src="{{ url('assets/js/stisla.js') }}"></script>
+
+    <!-- JS Libraies -->
+
+    <!-- Page Specific JS File -->
+
+    <!-- Template JS File -->
+    <script src="{{ url('assets/js/scripts.js') }}"></script>
+    <script src="{{ url('assets/js/custom.js') }}"></script>
+</body>
+
+</html>
